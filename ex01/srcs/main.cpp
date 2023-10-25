b9c208a30c598380b4a65aba8ce6deb2660be9d2
theo <sylvie.theo.mathieu@gmail.com>
2023-10-25 23:33:26 +0200

    Ex01 almost finished

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tduprez <tduprez@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 00:04:16 by tduprez           #+#    #+#             */
-/*   Updated: 2023/10/25 15:13:46 by tduprez          ###   ########lyon.fr   */
+/*   Updated: 2023/10/25 22:12:59 by tduprez          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,43 @@
 
 int	main(void)
 {
-	Span	span(10);
-	for (int i = 0; i < 15; i++)
 	{
+		Span	span(10);
+		for (int i = 0; i < 15; i++)
+		{
+			try
+			{
+				span.addNumber(i);
+				std::cout << span._data[i] << " --- " << i << std::endl;
+			}
+			catch (std::range_error& e)
+			{
+				std::cout << e.what() << std::endl;
+			}
+		}
+		std::cout << span._data.size() << std::endl;
+		for (int i = 0; i < 10; i++)
+			std::cout << span._data[i] << std::endl;
+	}
+
+	{
+		Span	span(100);
+		std::vector<int> base;
+
 		try
 		{
-			span.addNumber(i);
+			span.addNumbersByRange(0, 1000);
 		}
-		catch (std::range_error& e)
+		catch(const std::out_of_range& e)
 		{
 			std::cout << e.what() << std::endl;
 		}
+		
+
+		// for (int i = 0; i < 1200; i++)
+		// 	std::cout << span._data[i] << std::endl;
 	}
+
+	
 	return 0;
 }
